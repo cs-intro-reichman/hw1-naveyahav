3ba66e273deb7aab131dd43548490f65b5cc3e1c
naveyahav <nave.yahav@post.runi.ac.il>
2024-11-07 17:09:07 +0200

    after 4

--- Bill3.java
+++ Bill3.java
@@ -2,8 +2,6 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program
-		System.out.println("please insert your names, and the total bill" + 
-			"by this order");
 	    String name1 = args[0];
 	    String name2 = args[1];
 		String name3 = args[2];
