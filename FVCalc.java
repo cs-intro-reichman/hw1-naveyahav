e7ac4e74e59f604f1895544a434a6045ba715383
naveyahav <nave.yahav@post.runi.ac.il>
2024-11-07 17:03:03 +0200

    after 4

--- FVCalc.java
+++ FVCalc.java
@@ -7,7 +7,8 @@ public class FVCalc {
 		Double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 		double futureValue = currentValue * Math.pow(1 + rate/100, n); 
-		System.out.println("After " + n + " years, " + "$" + currentValue  + 
-		 	" saved at " + rate + "% " + "will yield " + " $" + (int) futureValue);
-	}
+		System.out.println("After " + n + " years, a " + "$" + currentValue  + 
+		 	" saved at " + rate + "% " + "will yield" + " $" + (int) futureValue);
+	} 
+	//After 2 years, a $100 saved at 10.0%25 will yield $121
 }
--- NumWords.java
+++ NumWords.java
@@ -1,7 +1,6 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-		System.out.println("please enter a number");
 		int hundreds, tens, ones;
 		int originalNum = Integer.parseInt(args[0]);
 		hundreds = originalNum / 100;
