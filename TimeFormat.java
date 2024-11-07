f47e1a50edf32534c3b40f508016e7724580fecb
naveyahav <nave.yahav@post.runi.ac.il>
2024-11-07 18:05:47 +0200

    final?

--- TimeFormat.java
+++ TimeFormat.java
@@ -13,7 +13,9 @@ public class TimeFormat {
 		// Does the same with the minutes part of the input.
 		String minutes = "" + args[0].charAt(3) + args[0].charAt(4);
         if (hours >= 12 && hours < 24) {
-			System.out.println(hours-12 + ":" + minutes + " PM");
+			if (hours != 12)
+				System.out.println(hours-12 + ":" + minutes + " PM");
+			else {System.out.println(hours + ":" + minutes + " PM"); }
 		} else {
 			System.out.println(hours + ":" + minutes + " AM");
 		}
